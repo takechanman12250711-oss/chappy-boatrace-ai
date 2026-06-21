3ef2425460fd1c2133f145fdfe2c1cbab7a595a6
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-06-21 17:42:13 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -251,7 +251,7 @@ function init(){
 
   console.log(data);
 
-  $("paste").value = data.officialText || data.officialHtml || "";
+  $("paste").$("paste").value = data.officialText || data.officialHtml || "";
   analyze(true);
 });
  applyVenue();
