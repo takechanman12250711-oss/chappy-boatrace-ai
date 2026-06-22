0cf6054b471d566511e54994e9e65fa5ee956441
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-06-22 18:33:19 +0900

    Simplify user-agent header in fetch request

--- api/odds.js
+++ api/odds.js
@@ -9,9 +9,9 @@ module.exports = async function handler(req, res) {
   try {
     const r = await fetch(url, {
   headers: {
-    "user-agent":
-      "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 Safari/605.1.15"
-    });
+    "user-agent": "Mozilla/5.0"
+  }
+});
     const html = await r.text();
 
     const text = html
