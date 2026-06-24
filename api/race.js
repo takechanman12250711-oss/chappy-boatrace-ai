d27b1a6c2234a6877bd7556bc3c0de521c1d2461
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-06-24 18:47:44 +0900

    Update race.js

--- api/race.js
+++ api/race.js
@@ -1,7 +1,7 @@
 // api/race.js v5.1
 // v5 + 全国24場プロファイル入り版
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
