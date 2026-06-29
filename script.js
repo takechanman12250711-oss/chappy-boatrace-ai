6d5270503a37bfc4ad12fda514b1d2d593d261aa
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-06-29 20:19:15 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -974,7 +974,10 @@ function calcBoatScore(b) {
   if (num(b.exhibitionST, 0) > 0 && num(b.exhibitionST) <= 0.12) s += 8;
   if (num(b.exhibitionTime, 0) > 0 && num(b.exhibitionTime) <= 6.75) s += 8;
   if (num(b.lapTime, 0) > 0 && num(b.lapTime) <= 37.00) s += 10;
-
+  if (num(b.exhibitionTime, 0) > 0 && num(b.exhibitionTime) >= 6.90) s -= 3;
+  if (num(b.lapTime, 0) > 0 && num(b.lapTime) >= 37.20) s -= 3;
+  if (num(b.motor2Rate, 0) > 0 && num(b.motor2Rate) <= 25) s -= 4;
+  
   return clamp(s);
 }
 
