dbc5ee73a8ff3e099621a899bb7c5f16d75b28e1
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-07-03 22:08:14 +0900

    render.js を更新

--- js/render.js
+++ js/render.js
@@ -9,18 +9,7 @@ function aiRankComment(score) {
   if (score >= 70) return "▲連下";
   return "☆穴";
 }
-function renderAiRank(aiRank = []) {
-  return aiRank
-    .map((x, i) => `
-      <p>
-        ${i + 1}位　
-        ${x.boat}号艇 ${x.name}
-        <b>${x.score}点</b>
-        (${aiRankComment(x.score)})
-      </p>
-    `)
-    .join("");
-}
+
 function renderTenkaiRate(tenkai) {
   return `
     <div class="race-line">
