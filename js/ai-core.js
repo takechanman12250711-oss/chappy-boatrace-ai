8a5b4d1c2677a77893aaadd337aae149ce056cff
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-07-12 14:59:45 +0900

    ai-core.js を更新

--- js/ai-core.js
+++ js/ai-core.js
@@ -3079,7 +3079,7 @@ aiComment: comment
       `%cチャッピーボートレースAI ${CORE_VERSION}`,
       "color:#2196f3;font-weight:bold;"
     );
-    console.log("ai-core.js v3.0.2-flow-fixed 読み込み完了");
+    console.log("ai-core.js v3.0.3-merge-fixed 読み込み完了");
   }
 
   /* ===============================
