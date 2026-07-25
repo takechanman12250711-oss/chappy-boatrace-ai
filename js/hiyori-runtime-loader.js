8a2ed3cdb5a26de7b8ccba7c946546e5cf54889b
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-07-25 17:17:01 +0900

    Remove duplicate scenario bridge from runtime loader

--- js/hiyori-runtime-loader.js
+++ js/hiyori-runtime-loader.js
@@ -13,7 +13,6 @@
     "js/prediction-skill-local-support.js",
     "js/prediction-motor-engine-support.js",
     "js/prediction-engine-integration.js",
-    "js/prediction-scenario-output-bridge.js",
     "js/prediction-simple-evaluation.js",
     "js/hiyori-event-monitor.js",
     "js/hiyori-learning-snapshot.js",
