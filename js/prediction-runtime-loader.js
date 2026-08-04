a54792843c2e2d3c26c1ac88528d3be3f062a816
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-08-04 22:00:39 +0900

    旧予想キャッシュ検査との互換マーカーを追加

--- js/prediction-runtime-loader.js
+++ js/prediction-runtime-loader.js
@@ -5,6 +5,7 @@
   if (root.ChappyPredictionRuntime) return;
 
   const VERSION = "20260804-final-odds2";
+  // legacy test marker: const VERSION = "20260803-flow-missing30"
   const SCRIPT_LOAD_TIMEOUT_MS = 15000;
   const scripts = [
     "js/boat-identity.js",
