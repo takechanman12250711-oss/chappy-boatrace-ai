fb81a5cbad219e9421fb2a6a3cac8ebd27a72cd0
takechanman12250711-oss <takechanman12250711@gmail.com>
2026-08-22 18:00:48 +0900

    3-2救済テストを6/6成立条件へ修正

--- scripts/test-pair-32-rescue-fixed5.js
+++ scripts/test-pair-32-rescue-fixed5.js
@@ -4,7 +4,7 @@ global.window=global;
 function prediction(){return{analyses:[
 {boatNo:1,indexes:{st:0,exhibition:0,raceFlow:60},roleScores:{attack:0,hold:50,pickup:60}},
 {boatNo:2,indexes:{st:0,exhibition:0,raceFlow:55},roleScores:{attack:0,hold:45,pickup:50}},
-{boatNo:3,indexes:{st:1,exhibition:1,raceFlow:62},roleScores:{attack:2,hold:56,pickup:56}},
+{boatNo:3,indexes:{st:1,exhibition:1,raceFlow:62},roleScores:{attack:2,hold:56,pickup:57}},
 {boatNo:4,indexes:{st:0,exhibition:0,raceFlow:54},roleScores:{attack:0,hold:40,pickup:45}},
 {boatNo:5,indexes:{st:0,exhibition:0,raceFlow:53},roleScores:{attack:0,hold:39,pickup:44}},
 {boatNo:6,indexes:{st:0,exhibition:0,raceFlow:52},roleScores:{attack:0,hold:38,pickup:43}}],formations:{main:["1-2-3","1-3-2","1-4-3"],safety:["1-2-4","1-5-2"]}};}
